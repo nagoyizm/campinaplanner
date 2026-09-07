71f388b152f7deabd779a2767eb2b98213d7f193
Nagoyizm <andresvegapozas@gmail.com>
2026-09-06 21:27:15 -0300

    cambio token_jw

--- auth.config.ts
+++ auth.config.ts
@@ -1,7 +1,12 @@
 import type { NextAuthConfig } from 'next-auth'
 
 export const authConfig = {
-  session: { strategy: 'jwt' },
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+    maxAge: 30 * 24 * 60 * 60, // 30 días en segundos (hace la cookie persistente en disco)
+    updateAge: 24 * 60 * 60,   // Renueva el token automáticamente cada 24h si hay actividad
+  },
   pages: {
     signIn: '/login',
   },
